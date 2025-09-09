141ba25badd48aa407ba5b85b83ae6db963f57a5
tpersi <99231629+tpersi@users.noreply.github.com>
2025-09-09 22:06:38 +0000

    adiciona scripts `lint:check` e  `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>A Catarina é a melhor coisa que fizemos juntos, se concorda, reaja no whatsapp. Amo Vocês!</h1>
+  return (
+    <h1>
+      A Catarina é a melhor coisa que fizemos juntos, se concorda, reaja no
+      whatsapp. Amo Vocês!
+    </h1>
+  );
 }
 
 export default Home;
