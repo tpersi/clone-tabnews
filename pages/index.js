ef6577cf9b0ac8a3c673bdfac9774cce92517db9
tpersi <99231629+tpersi@users.noreply.github.com>
2025-09-09 13:51:54 +0000

    Atualiza frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>A Catarina é a melhor coisa que fizemos juntos, se concorda, reaja no whatsapp</h1>
+    return <h1>A Catarina é a melhor coisa que fizemos juntos, se concorda, reaja no whatsapp. Amo Vocês!</h1>
 }
 
 export default Home;
